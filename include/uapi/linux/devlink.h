1ee0c250970f3f0e83f2c42e24c357ebc93101dc
Vlad Buslov <vladbu@mellanox.com>
2019-12-10 21:44:01 +0200

    devlink: Allow setting parent group of subdevs and rate groups
    Refactor DEVLINK_CMD_SUBDEV_RATE_{GET|SET} command handlers to support
    setting a group as a parent for subdev or another group through attribute
    DEVLINK_ATTR_SUBDEV_RATE_PARENT.
    
    A child subdev or group doesn't need to be removed from parent group before
    attaching it to another parent group. This is done automatically by devlink
    API implementation. Also, if parent group is deleted, all of its orphaned
    children are automatically detached. To detach the child manually user must
    attach it to special "noparent" parent group ("noparent" is a shortcut for
    "parent noparent"). Group name "noparent" becomes reserved and no groups
    with that name can be created by users anymore.
    
    Example:
    
    $ devlink subdev rate add netdevsim/netdevsim10/group1
    
    $ devlink subdev rate add netdevsim/netdevsim10/group2
    
    $ devlink subdev rate set netdevsim/netdevsim10/group1 type node parent group2
    
    $ devlink subdev rate show netdevsim/netdevsim10/group1 type node
    netdevsim/netdevsim10/group1: type node parent group2
    
    $ devlink subdev rate show netdevsim/netdevsim10/group1 type node -jp
    {
        "subdev rate": {
            "netdevsim/netdevsim10/group1": {
                "type": "node",
                "parent": "group2"
            }
        }
    }
    
    $ devlink subdev rate set netdevsim/netdevsim10/group1 type node noparent
    
    Signed-off-by: Vlad Buslov <vladbu@mellanox.com>

--- include/uapi/linux/devlink.h
+++ include/uapi/linux/devlink.h
@@ -457,6 +457,7 @@ enum devlink_attr {
 	DEVLINK_ATTR_SUBDEV_RATE_MIN_TX,	/* u32 */
 	DEVLINK_ATTR_SUBDEV_RATE_MAX_TX,	/* u32 */
 	DEVLINK_ATTR_SUBDEV_RATE_GROUP_NAME,	/* string */
+	DEVLINK_ATTR_SUBDEV_RATE_PARENT,	/* string */
 
 	/* add new attributes above here, update the policy in devlink.c */
 
