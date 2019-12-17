9aa251ea213d5b0b8a02703fb542067cd042c955
Yuval Avnery <yuvalav@mellanox.com>
2019-12-17 09:20:38 +0200

    devlink: Introduce slice
    Slice represents a device that exists on the ASIC but is not necessarily
    visible to the kernel.
    
    Example:
    
    A privileged user wants to configure a VF's hw_addr, before the VF is
    enabled.
    
    $ devlink slice set pci/0000:03:00.0/1 hw_addr 10:22:33:44:55:66
    
    $ devlink slice show pci/0000:03:00.0/1
    pci/0000:03:00.0/1: flavour pcivf pfnum 0 vfnum 0 port_index 1 hw_addr 10:22:33:44:55:66
    
    $ devlink slice show pci/0000:03:00.0/1 -jp
    {
        "slice": {
            "pci/0000:03:00.0/1": {
                "flavour": "pcivf",
                "pfnum": 0,
                "vfnum": 0,
                "port_index": 1,
                "hw_addr": "10:22:33:44:55:66"
    	}
        }
    }
    
    Change-Id: I6b43b43abe4dfb8cf99fc0bed19f2debf510bd22
    Signed-off-by: Yuval Avnery <yuvalav@mellanox.com>
    Suggested-by: Parav Pandit <parav@mellanox.com>

--- include/uapi/linux/devlink.h
+++ include/uapi/linux/devlink.h
@@ -117,6 +117,11 @@ enum devlink_command {
 	DEVLINK_CMD_TRAP_GROUP_NEW,
 	DEVLINK_CMD_TRAP_GROUP_DEL,
 
+	DEVLINK_CMD_SLICE_GET,		/* can dump */
+	DEVLINK_CMD_SLICE_SET,
+	DEVLINK_CMD_SLICE_NEW,
+	DEVLINK_CMD_SLICE_DEL,
+
 	/* add new commands above here */
 	__DEVLINK_CMD_MAX,
 	DEVLINK_CMD_MAX = __DEVLINK_CMD_MAX - 1
@@ -187,6 +192,11 @@ enum devlink_port_flavour {
 				      * for the PCI VF. It is an internal
 				      * port that faces the PCI VF.
 				      */
+};
+
+enum devlink_slice_flavour {
+	DEVLINK_SLICE_FLAVOUR_PCI_PF,
+	DEVLINK_SLICE_FLAVOUR_PCI_VF,
 };
 
 enum devlink_param_cmode {
@@ -426,6 +436,13 @@ enum devlink_attr {
 	DEVLINK_ATTR_NETNS_FD,			/* u32 */
 	DEVLINK_ATTR_NETNS_PID,			/* u32 */
 	DEVLINK_ATTR_NETNS_ID,			/* u32 */
+
+	DEVLINK_ATTR_SLICE_INDEX,		/* u32 */
+	DEVLINK_ATTR_SLICE_FLAVOUR,		/* u16 */
+	DEVLINK_ATTR_SLICE_PF_INDEX,		/* u32 */
+	DEVLINK_ATTR_SLICE_VF_INDEX,		/* u32 */
+	DEVLINK_ATTR_SLICE_HW_ADDR,		/* binary */
+
 	/* add new attributes above here, update the policy in devlink.c */
 
 	__DEVLINK_ATTR_MAX,
